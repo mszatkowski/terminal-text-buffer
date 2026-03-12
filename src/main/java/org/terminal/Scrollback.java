4837fa38a204a097e2b361a0c851bee7130be1be
Marcin <hulkcinek@gmail.com>
2026-03-12 02:14:35 +0100

    Basic implementation of Scrollback

--- src/main/java/org/terminal/Scrollback.java
+++ src/main/java/org/terminal/Scrollback.java
@@ -1,5 +1,37 @@
 package org.terminal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Scrollback {
 
+    private final int maxLines;
+    private final int width;
+    private final List<Line> lines;
+
+    public Scrollback(int maxLines, int width) {
+        this.maxLines = maxLines;
+        this.width = width;
+        this.lines = new ArrayList<>();
+    }
+
+    void push(Line line) {
+        lines.add(line);
+
+        if (lines.size() > maxLines) {
+            lines.removeFirst();
+        }
+    }
+
+    Line getLine(int index) {
+        return lines.get(getSize() + index);
+    }
+
+    int getSize() {
+        return lines.size();
+    }
+
+    int getWidth() {
+        return width;
+    }
 }
