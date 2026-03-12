592958167d4bec8f2f3de5adba3c992efdf64545
Marcin <hulkcinek@gmail.com>
2026-03-12 01:21:05 +0100

    Basic implementation of Line

--- src/main/java/org/terminal/Line.java
+++ src/main/java/org/terminal/Line.java
@@ -1,5 +1,20 @@
 package org.terminal;
 
 class Line {
+    private final Cell[] cells;
 
+    Line(int width) {
+        cells = new Cell[width];
+        for (int i = 0; i < width; i++) {
+            cells[i] = new Cell('\0');
+        }
+    }
+
+    Cell getCell(int index) {
+        return cells[index];
+    }
+
+    void setCell(int index, Cell cell) {
+        cells[index] = cell;
+    }
 }
