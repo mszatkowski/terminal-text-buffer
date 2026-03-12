449b4ec0e28b09ee9b8a5c6dadbc97e469c2482e
Marcin <hulkcinek@gmail.com>
2026-03-12 01:33:41 +0100

    Basic implementation of Screen

--- src/main/java/org/terminal/Screen.java
+++ src/main/java/org/terminal/Screen.java
@@ -2,4 +2,32 @@ package org.terminal;
 
 class Screen {
 
+    private final int width;
+    private final int height;
+    private final Line[] lines;
+
+    Screen(int width, int height) {
+        this.width = width;
+        this.height = height;
+        lines = new Line[height];
+        for (int i = 0; i < height; i++) {
+            lines[i] = new Line(width);
+        }
+    }
+
+    Cell getCell(int x, int y) {
+        return lines[y].getCell(x);
+    }
+
+    Line getLine(int y) {
+        return lines[y];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
